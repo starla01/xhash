e4d974f27cc7f0e1aa04edb048e5b33c4dff5348
Fernando Robles Rivera <fernando.ro.r@hotmail.com>
2019-02-09 02:04:06 -0600

    add fonts & Layout styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './index.module.sass';
 
+const min = 0;
+const max = 4;
+
 const Header = () => {
 	return <section className={styles.header}>
-		Header
+		<div className={styles.logo}>
+			XHASH
+		</div>
+		<div className={styles.pagination}>
+			<div className={styles.page}>1</div>
+			<div className={styles.page}>2</div>
+			<div className={styles.page}>3</div>
+			<div className={styles.page}>4</div>
+		</div>
 	</section>
 };
 
